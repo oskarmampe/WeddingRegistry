4ff8964a58b1376f86bb76f857be1d6f05837928
Oskar Mampe <oskarmampe@hotmail.co.uk>
2019-01-15 19:19:12 +0000

    Add project files.

---
+++ WeddingRegistry/LoginActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace WeddingRegistry
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/AppTheme.NoActionBar")]
+    public class LoginActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_login);
+
+            FindViewById<TextView>(Resource.Id.app_bar_tv_title).Text = "Login";
+
+            Button button = FindViewById<Button>(Resource.Id.login_btn_submit);
+            button.Click += Login;
+        }
+
+        private void Login(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "Clicked", ToastLength.Short).Show();
+        }
+    }
+}
---
+++ WeddingRegistry/RegistryActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace WeddingRegistry
+{
+	[Activity(Label = "@string/wedding_item_title", Theme = "@style/AppTheme.NoActionBar")]
+	public class RegistryActivity : AppCompatActivity
+	{
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_main);
+
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
+        }
+
+        //public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //    int id = item.ItemId;
+        //    if (id == Resource.Id.action_settings)
+        //    {
+        //        return true;
+        //    }
+
+        //    return base.OnOptionsItemSelected(item);
+        //}
+
+	}
+}
+
